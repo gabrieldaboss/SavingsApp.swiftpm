1ba8e7fb13b1e0cf398b09ef73633edd4f08f8ce
Michael Mosier <mmosier8293@stu.d214.org>
2025-04-14 11:12:01 -0500

    added imagw

--- ContentView.swift
+++ ContentView.swift
@@ -3,7 +3,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "dollarsign.arrow.trianglehead.counterclockwise.rotate.90")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
