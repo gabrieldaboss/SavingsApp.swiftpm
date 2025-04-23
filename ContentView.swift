f36175ad8ea35d709d5755a4b114422a9d877bb3
Gabriel xcode <grusinski8380@stu.d214.org>
2025-04-23 08:31:14 -0700

    started off code in playgrounds created new @States and got a working savings calc as well as some new code making buttons that can withdraw your money and add more to your account.

--- ContentView.swift
+++ ContentView.swift
@@ -1,42 +1,71 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var number1 = ("")
-    @State var number2 = ("")
-    @State var number3 = ("")
-    @State var number4 = ("")
+    @State var number1 = ""
+    @State var number2 = ""
+    @State var number3 = ""
+    @State var number4 = ""
+    @State var totalSavings = ""
+
     var body: some View {
         VStack {
+            
             Image(systemName: "dollarsign.ring")
-                .padding(.top,-315)
+                .font(.system(size: 80))
+                .foregroundColor(.green)
+                .padding(.top, 40)
+
+            
+            Text("Savings App")
                 .font(.largeTitle)
                 .foregroundColor(.green)
-            Text("Savings App")
-                .padding(.top, -360)
-                .font(Font.largeTitle)
-                
-                .foregroundColor(.green)
-            
-                .foregroundColor(.accentColor)
-  
-            
-            
-            HStack{
+                .padding(.bottom, 30)
 
-                TextField("Savings",text: $number1)
             
-            
-                TextField("Enter det",text: $number2)
-                
-            TextField("spending $",text: $number3)
-                
-                TextField("adding $",text: $number4)
-        
-            
+            TextField("How much money you have", text: $number1)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.horizontal)
 
+
+            TextField("How much you spent", text: $number3)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.horizontal)
+
+            TextField("How much you added", text: $number4)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.horizontal)
+
+            
+            Button("Add it up!") {
+                calculateTotal()
             }
+            .padding()
+            .background(Color.green)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+            .padding(.top, 20)
+
+            
+            Text("Total Savings: \(totalSavings)")
+                .font(.title)
+                .padding(.top, 20)
+
+            Spacer()
         }
-        
     }
-       
+
+    
+    func calculateTotal() {
+        if let start = Double(number1),
+           let spend = Double(number3),
+           let add = Double(number4) {
+
+            let total = start - spend + add
+            totalSavings = String(total)
+        } else {
+            totalSavings = "Type numbers only!"
+        }
+    }
 }
+
+
