cb42cc57f19864ebf421311ced612410d56be911
AydinMetanovic <ametanovic8283@stu.d214.org>
2025-04-29 10:07:37 -0500

    i made the background so its green and white wtih a nice look fading away

--- ContentView.swift
+++ ContentView.swift
@@ -6,104 +6,112 @@ struct ContentView: View {
     @State var number4 = ""
     @State var totalSavings = ""
     @State var rotation = 0.0
-   
-
-
     
     
-
+    
+    
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "dollarsign.ring")
-                .font(.system(size: 80))
-                .foregroundColor(.green)
-
-                .padding(.top, 20)
-
-
-                .padding(.top, 40)
+        ZStack{
+            LinearGradient(
+                gradient: Gradient(colors: [.white,.green.opacity(0.2)]),
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .ignoresSafeArea()
             
-            
-
-            Text("Savings App")
-                .font(.largeTitle)
-                .foregroundColor(.green)
-                .padding(.bottom, 30)
-            
-            ZStack{
-                Circle()
-                    .fill(Color.green.opacity(0.1))
-                    .frame(width: 150,height: 150)
-                    .shadow(radius: 5)
-                Circle()
-                    .stroke(Color.green,lineWidth: 5)
-                    .frame(width: 150,height: 150)
-                
-                Text(totalSavings)
-                    .font(.title)
-                    .bold()
+            VStack {
+                Image(systemName: "dollarsign.ring")
+                    .font(.system(size: 80))
                     .foregroundColor(.green)
-            }
-            .padding(.vertical,20)
-            TextField("How much money you have", text: $number1)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .keyboardType(.decimalPad)
-                .padding(.horizontal)
-
-
-
-            
-            
-
-            TextField("How much you spent", text: $number3)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .keyboardType(.decimalPad)
-                .padding(.horizontal)
-            
-            TextField("How much you added", text: $number4)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .keyboardType(.decimalPad)
-                .padding(.horizontal)
-
-            
-            
-
-            Button("Add it up!") {
-                calculateTotal()
-            }
-            .padding()
-            .background(Color.green)
-            .foregroundColor(.white)
-            .cornerRadius(10)
-            .padding(.top, 20)
-
-
-
-            
-            
-            Text("Total Savings: \(totalSavings)")
-                .font(.title)
-                .padding(.top, 20)
-            
-          
-                }
-            }
-            
-            
-            func calculateTotal() {
-                if let start = Double(number1),
-                   let spend = Double(number3),
-                   let add = Double(number4) {
-                    
-                    let total = start - spend + add
-                    totalSavings = String(total)
-                } else {
-                    totalSavings = "Type numbers only!"
-                }
                 
+                    .padding(.top, 20)
+                
+                
+                    .padding(.top, 40)
+                
+                
+                
+                Text("Savings App")
+                    .font(.largeTitle)
+                    .foregroundColor(.green)
+                    .padding(.bottom, 30)
+                
+                ZStack{
+                    Circle()
+                        .fill(Color.green.opacity(0.1))
+                        .frame(width: 150,height: 150)
+                        .shadow(radius: 5)
+                    Circle()
+                        .stroke(Color.green,lineWidth: 5)
+                        .frame(width: 150,height: 150)
+                    
+                    Text(totalSavings)
+                        .font(.title)
+                        .bold()
+                        .foregroundColor(.green)
+                }
+                .padding(.vertical,20)
+                TextField("How much money you have", text: $number1)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .keyboardType(.decimalPad)
+                    .padding(.horizontal)
+                
+                
+                
+                
+                
+                
+                TextField("How much you spent", text: $number3)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .keyboardType(.decimalPad)
+                    .padding(.horizontal)
+                
+                TextField("How much you added", text: $number4)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .keyboardType(.decimalPad)
+                    .padding(.horizontal)
+                
+                
+                
+                
+                Button("Add it up!") {
+                    calculateTotal()
+                }
+                .padding()
+                .background(Color.green)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+                .padding(.top, 20)
+                
+                
+                
+                
+                
+                Text("Total Savings: \(totalSavings)")
+                    .font(.title)
+                    .padding(.top, 20)
+                
+            }
             }
         }
-
+        
+        
+        func calculateTotal() {
+            if let start = Double(number1),
+               let spend = Double(number3),
+               let add = Double(number4) {
+                
+                let total = start - spend + add
+                totalSavings = String(total)
+            } else {
+                totalSavings = "Type numbers only!"
+            }
+            
+        }
+    }
+    
 
 
 
