e86a288d588d4667d6dcb5145a29922f66fbafca
AydinMetanovic <ametanovic8283@stu.d214.org>
2025-05-13 11:11:51 -0500

    making it work

--- ContentView.swift
+++ ContentView.swift
@@ -10,6 +10,7 @@ struct ContentView: View {
     var body: some View {
         NavigationStack{
             ZStack{
+//Test
                 LinearGradient(
                     gradient: Gradient(colors: [.white,.green.opacity(0.4)]),
                     startPoint: .top,
